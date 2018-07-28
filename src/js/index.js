1cd030cd055042d49698c0ef77b09ebdc38921da
Julio <jforerom@ucenfotec.ac.cr>
2018-07-28 15:58:55 -0600

    Netlify form handling

--- src/js/index.js
+++ src/js/index.js
@@ -7,28 +7,42 @@ let closeModalBtn = document.querySelector('#close-modal-btn');
 let modal = document.querySelector('#modal');
 let recommendedBlogs = document.querySelector('.recommended__blogs');
 
-let blogPostTemplate = document.querySelector("#blog-post-template");
-let recommendedBlogTemplate = document.querySelector("#recommended-blog-template");
+let blogPostTemplate = document.querySelector('#blog-post-template');
+let recommendedBlogTemplate = document.querySelector('#recommended-blog-template');
+let blogPostImageContentTemplate = document.querySelector("#blog-post-image-content-template");
+let blogPostTextContentTemplate =  document.querySelector("#blog-post-text-content-template");
+let ownBlogPostTemplate = document.querySelector("#own-blog-post-template");
 
-let main = document.querySelector("main");
+let main = document.querySelector('main');
+let menu = document.querySelector('#menu');
+let textPostForm = document.querySelector('#text-post-form');
+
+textPostForm.addEventListener('submit', (e)=>{
+
+    if(!textContent.style.display || textContent.style.display === 'none'){
+        console.log('Cancelled');
+        e.preventDefault();
+        return false;
+    }
+});
 
 closeModalBtn.addEventListener('click', (e)=>{
     e.preventDefault();
     modal.style.display = 'none';
-    photoContent.style.display = "none";
-    textContent.style.display = "none";
+    photoContent.style.display = 'none';
+    textContent.style.display = 'none';
 });
 
 postPhoto.addEventListener('click', (e)=>{
     e.preventDefault();
-    modal.style.display = "block";
-    photoContent.style.display = "flex";
+    modal.style.display = 'block';
+    photoContent.style.display = 'flex';
 });
 
 postText.addEventListener('click', (e)=>{
     e.preventDefault();
-    modal.style.display = "block";
-    textContent.style.display = "flex";
+    modal.style.display = 'block';
+    textContent.style.display = 'flex';
 });
 
 function get(url, callback){
@@ -41,16 +55,29 @@ function get(url, callback){
       });
 }
 
+get('https://api.netlify.com/api/v1/sites/adoring-leakey-915c67.netlify.com/submissions?access_token=efd8f49eea411ca23ebaaab6340a79417afd1c4dc52919e2c97652a879334095',(j)=>{
+    let blogPost;
+    for(let i = 0; i < j.length; i++){
+        blogPost = ownBlogPostTemplate.content.cloneNode(true);
+        blogPost.querySelector('.blog-post__content').appendChild(blogPostTextContentTemplate.content.cloneNode(true));
+        blogPost.querySelector('.blog-post__content__title').innerHTML = j[i].data.title;
+        blogPost.querySelector('.blog-post__content__text').innerHTML = j[i].data.content;
+        blogPost.querySelector('.blog-post__tags').innerHTML = j[i].data.tags;
+        menu.after(blogPost);
+    }
+});
+
 get('api/posts.json',(j)=>{
 
     let blogPost;
     for(let i = 0; i < j.length; i++){
         blogPost = blogPostTemplate.content.cloneNode(true);
-        blogPost.querySelector(".profile-img-lg").src = j[i].author.avatar;
-        blogPost.querySelector(".blog-post__author").innerHTML = j[i].author.name;
-        blogPost.querySelector(".blog-post__image-content").src = j[i].content;
-        blogPost.querySelector(".blog-post__notes span").innerHTML = j[i].notes;
-        blogPost.querySelector(".blog-post__tags").innerHTML = j[i].tags.join(' ');
+        blogPost.querySelector('.profile-img-lg').src = j[i].author.avatar;
+        blogPost.querySelector('.blog-post__author').innerHTML = j[i].author.name;
+        blogPost.querySelector('.blog-post__content').appendChild(blogPostImageContentTemplate.content.cloneNode(true));
+        blogPost.querySelector('.blog-post__image-content').src = j[i].content;
+        blogPost.querySelector('.blog-post__notes span').innerHTML = j[i].notes;
+        blogPost.querySelector('.blog-post__tags').innerHTML = j[i].tags.join(' ');
         main.appendChild(blogPost);
     }
 
@@ -61,9 +88,9 @@ get('api/recommended_blogs.json',(j)=>{
     let recommendedBlog;
     for(let i = 0; i < j.length; i++){
         recommendedBlog = recommendedBlogTemplate.content.cloneNode(true);
-        recommendedBlog.querySelector(".profile-img-sm").src = j[i].author.avatar;
-        recommendedBlog.querySelector(".blog-post__author").innerHTML = j[i].author.name;
-        recommendedBlog.querySelector(".recommended__blog__name").innerHTML = j[i].blogName;
+        recommendedBlog.querySelector('.profile-img-sm').src = j[i].author.avatar;
+        recommendedBlog.querySelector('.blog-post__author').innerHTML = j[i].author.name;
+        recommendedBlog.querySelector('.recommended__blog__name').innerHTML = j[i].blogName;
         recommendedBlogs.appendChild(recommendedBlog);
     }
 
